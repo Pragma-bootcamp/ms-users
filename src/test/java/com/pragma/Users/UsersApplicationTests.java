20d24fe7834dd2b6e1e0b4c53b563ca0847596de
Marcos Vargas <marcos.developer99@gmail.com>
2024-08-19 15:16:19 -0500

    feat: Adding initial base project

---
+++ src/test/java/com/pragma/Users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
